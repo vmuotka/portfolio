c48200f1c718a08f315b551302b9aec84f65ae72
Jakk3 <32640696+Jakk3@users.noreply.github.com>
2020-11-23 16:40:03 +0200

    fix project details button showing when there are none

--- client/src/views/Projects.js
+++ client/src/views/Projects.js
@@ -11,14 +11,16 @@ const Projects = () => {
       desc: "A web app to track user's watch progress of TV shows, record watched shows and list shows that the user is planning to watch. Includes user profiles, a search function for both TV shows and users and a page to discover new shows based on the user's list of TV shows.",
       cover: mytvlist,
       link: 'https://vmuotka-mytvlist.herokuapp.com/user/jakk3',
-      github: 'https://github.com/vmuotka/mytvlist'
+      github: 'https://github.com/vmuotka/mytvlist',
+      details: true
     },
     {
       title: 'Ketunkolo',
       desc: 'A DnD (Dungeons&Dragons) tool web app. Contains an initiative tracker (for DnD combat) and a search for DnD spells and creatures. Users can also save their initiative tracker setups and create their own creatures, that are saved for all to use.',
       cover: ketunkolo,
       link: 'https://vmuotka-ketunkolo.herokuapp.com',
-      github: 'https://github.com/vmuotka/ketunkolo'
+      github: 'https://github.com/vmuotka/ketunkolo',
+      details: false
     }
   ]
   return (
